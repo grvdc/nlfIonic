3834863cb3553291fb8b030015f20c7d656fc43d
gaurav-ocodewire <gauravk@ocodewire.com>
2020-02-04 17:08:48 +0530

    working shop page to product detail page

--- src/app/pages/shop-page/shop-page.page.ts
+++ src/app/pages/shop-page/shop-page.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationExtras, Router } from '@angular/router';
 
 @Component({
   selector: 'app-shop-page',
@@ -6,10 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shop-page.page.scss'],
 })
 export class ShopPagePage implements OnInit {
-
-  constructor() { }
+  testProductDetail = {
+    id: 1,
+    img: 'https://rukminim1.flixcart.com/image/714/857/k47cgi80/jacket/e/z/k/s-58452101-puma-original-imafn2dvgprxsvsb.jpeg?q=50',
+    price: 25,
+    jacketType: 'womenJackets',
+    headerType:'Women Jacket',
+    title:'Full Sleeve Solid Women Jacket',
+    discription: 'A jacket is a mid-stomach–length garment for the upper body. A jacket typically has sleeves.',
+    rate:3,
+    productimages:[
+      'https://rukminim1.flixcart.com/image/714/857/k47cgi80/jacket/e/z/k/s-58452101-puma-original-imafn2dvkjh5rm52.jpeg?q=50',
+      'https://rukminim1.flixcart.com/image/714/857/k47cgi80/jacket/e/z/k/s-58452101-puma-original-imafn2dvgprxsvsb.jpeg?q=50',
+      'https://rukminim1.flixcart.com/image/714/857/k47cgi80/jacket/e/z/k/s-58452101-puma-original-imafn2dvmkhggq3f.jpeg?q=50',
+      'https://rukminim1.flixcart.com/image/714/857/k47cgi80/jacket/e/z/k/s-58452101-puma-original-imafn2dvzw8nfhnw.jpeg?q=50',
+    ]
+  }
+  constructor(
+    private router: Router,
+  ) { }
 
   ngOnInit() {
   }
 
+  testDetail() {
+    console.log('datatttt',this.testProductDetail);
+    let navigationExtras: NavigationExtras = {
+      queryParams: {
+        testProductDetail: JSON.stringify(this.testProductDetail)
+      }
+    };
+    this.router.navigate(['product-detail-page'], navigationExtras);
+  }
 }
